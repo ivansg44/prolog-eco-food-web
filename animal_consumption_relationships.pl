2d78cbd551116af7a8e3fb9f5c88a5a24d947049
Ivan S Gill <isgill93@student.ubc.ca>
2021-04-07 22:06:44 -0700

    Recursive predator list
    * Removed append_without_duplicating

--- animal_consumption_relationships.pl
+++ animal_consumption_relationships.pl
@@ -64,13 +64,36 @@ prey_helper(Predator1, [consumption(Prey, Predator2, _)|T1], T2) :-
     dif(Predator1, Predator2),
     prey_helper(Predator1, T1, T2).
 
-% append_without_duplicating(List1, List2, List3) is True if List3 is equal the result of adding all elements from List2 that are not already in List1, to the end of List1.
-append_without_duplicating(List1, [], List1).
-append_without_duplicating(List1, [H2|T2], List3) :-
-    member(H2, List1),
-    append_without_duplicating(List1, T2, List3).
-append_without_duplicating(List1, [H2|T2], List3) :-
-    not(member(H2, List1)),
-    append(List1, [H2], NewList1),
-    append_without_duplicating(NewList1, T2, List3).
+% recursive_predators(Prey, RecursivePredators) is true if RecursivePredators
+% is a list of animals higher up on the food chain than Prey.
+recursive_predators(Prey, RecursivePredators) :-
+    recursive_predators_helper([Prey], RecursivePredators, [Prey]).
+
+% recursive_predators_helper([Prey], RecursivePredators, VisitedAnimals) is
+% true if RecursivePredators is a list recursive predators of Prey representing
+% a successful iteration through the food web, using VistedAnimals as an
+% accumulator.
+recursive_predators_helper([], [], _).
+recursive_predators_helper([H1|T1], RecursivePredators, VisitedAnimals) :-
+    predators(H1, []),
+    recursive_predators_helper(T1, RecursivePredators, VisitedAnimals).
+recursive_predators_helper([H1|T1], RecursivePredators, VisitedAnimals) :-
+    predators(H1, P1),
+    subtract_list(P1, VisitedAnimals, FilteredP1),
+    append(T1, FilteredP1, T1FilteredP1),
+    append(VisitedAnimals, FilteredP1, NewVisitedAnimals),
+    recursive_predators_helper(T1FilteredP1,
+                               AlmostRecursivePredators,
+                               NewVisitedAnimals),
+    append(FilteredP1, AlmostRecursivePredators, RecursivePredators).
+
+% subtract_list(List1, List2, List3) is True if List3 is List1 with all its
+% elements also found in List2 removed.
+subtract_list([], _, []).
+subtract_list([H1|T1], List2, List3) :-
+    member(H1, List2),
+    subtract_list(T1, List2, List3).
+subtract_list([H1|T1], List2, [H1|List3]) :-
+    not(member(H1, List2)),
+    subtract_list(T1, List2, List3).
 
